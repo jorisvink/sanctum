0813555fab04014582c956bf482708e689a135ed
Joris Vink <joris@sanctorum.se>
2023-08-14 23:04:35 +0200

    move mail to sanctorum.se

--- src/chapel.c
+++ src/chapel.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Joris Vink <joris@coders.se>
+ * Copyright (c) 2023 Joris Vink <joris@sanctorum.se>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -108,7 +108,7 @@ sanctum_chapel(struct sanctum_proc *proc)
 			chapel_offer_create(now);
 		}
 
-		usleep(250000);
+		sleep(1);
 	}
 
 	syslog(LOG_NOTICE, "exiting");
