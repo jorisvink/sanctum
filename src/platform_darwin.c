48afcfba0bc1c85ea08780e3df3d7491ad9a488a
Joris Vink <joris@sanctorum.se>
2024-08-07 20:57:27 +0200

    Updates to apple sandboxing for chapel.
    Add the KEK path and the cathedral secret path specifically.

--- src/platform_darwin.c
+++ src/platform_darwin.c
@@ -255,8 +255,8 @@ sanctum_platform_sandbox(struct sanctum_proc *proc)
 {
 	struct stat	st;
 	size_t		flen;
-	int		fd, len;
-	const char	*params[8];
+	const char	*params[16];
+	int		fd, len, idx;
 	char		*profile, *errmsg, path[1024];
 
 	PRECOND(proc != NULL);
@@ -283,12 +283,28 @@ sanctum_platform_sandbox(struct sanctum_proc *proc)
 	if (len == -1 || (size_t)len >= sizeof(path))
 		fatal("failed to construct new path");
 
-	/* Construct all parameters that the profiles can use. */
-	params[0] = "KEY_PATH";
-	params[1] = sanctum->secret;
-	params[2] = "KEY_PATH_NEW";
-	params[3] = path;
-	params[4] = NULL;
+	/*
+	 * Construct all parameters that the profiles can use.
+	 * Note that it doesn't mean they will, for example only
+	 * chapel will ues KEY_PATH, KEK_PATH or CATHEDRAL_SECRET.
+	 */
+	idx = 0;
+	params[idx++] = "KEY_PATH";
+	params[idx++] = sanctum->secret;
+	params[idx++] = "KEY_PATH_NEW";
+	params[idx++] = path;
+
+	if (sanctum->cathedral_secret != NULL) {
+		params[idx++] = "CATHEDRAL_SECRET";
+		params[idx++] = sanctum->cathedral_secret;
+	}
+
+	if (sanctum->kek != NULL) {
+		params[idx++] = "KEK_PATH";
+		params[idx++] = sanctum->kek;
+	}
+
+	params[idx] = NULL;
 
 	/* Open the profile from disk. */
 	len = snprintf(path, sizeof(path), "%s/%s.sb",
